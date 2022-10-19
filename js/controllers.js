c619704db34b0d877363f160b0470c79a886f365
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-19 15:17:02 +0800

    added method to update a task

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,27 @@
-todoApp.controller('homeController', ['$scope', '$timeout', 'todoService', function($scope, $timeout, todoService){
-    $scope.message = "HOME";
+todoApp.controller('homeController', ['$scope', '$timeout', '$interval', 'todoService', function($scope, $timeout, $interval, todoService){
+    
+    // $interval(function(){
+    //     $scope.myTasks = todoService.getTodoData();
+    //     console.log(todoService.dataInitiliazed);
+    //     if(todoService.dataInitiliazed) return;
+    // }, 1000);
+
+    // wait for the to do data initialized
+    $timeout(function(){
+        $scope.myTasks = todoService.getTodoData();
+        // console.log($scope.myTasks);
+    }, 100);
+
 }]);
 
 todoApp.controller('createTodoController', ['$scope', '$timeout', 'todoService', function($scope, $timeout, todoService){
-    $scope.message = "CREATE";
+    $scope.message = "";
+    $scope.taskBody;
+    $scope.postToServer = function(){
+        todoService.postToServer($scope.taskBody);
+        $scope.message = "Success";
+    }
+
 }]);
 
 todoApp.controller('updateTodoController', ['$scope', '$timeout', '$routeParams', 'todoService', function($scope, $timeout, $routeParams, todoService){
--- js/services.js
+++ js/services.js
@@ -4,12 +4,13 @@ todoApp.service('todoService', function(){
     self.dataInitiliazed = false;
     self.initializeData = async function(){
         if(!self.dataInitiliazed){
-             self.dataInitiliazed = true;
+             
              let res = await fetch('http://127.0.0.1:8080/todo');
              let data = await res.json();
-             self.todoApp = data;
+             self.todoData = data;
              console.log("Todo service successfully initialized");
-             console.log(self.todoApp);
+             self.dataInitiliazed = true;
+            //  console.log(self.todoApp);
         }else{
             console.warn("Todo service has already been initialized");
         }
@@ -19,6 +20,39 @@ todoApp.service('todoService', function(){
     self.getTodoData = function(){
         if(self.dataInitiliazed) return self.todoData;
         console.error("Todo data hasnt been initialized");
+        // self.getTodoData();
         return -69;
+    }
+
+    self.postToServer = async taskBody =>{
+        let res = await fetch('http://127.0.0.1:8080/create-task', {
+            method:"POST",
+            body: JSON.stringify({
+                body:taskBody,
+                isDone:true,
+                creationDatetime:''
+            }),
+            headers:{
+                "Content-type": "application/json"
+            }
+        });
+        let data = await res.json();
+        self.dataInitiliazed = false;
+        self.initializeData();
+        return data;
+    }
+
+    self.updateTodo = async todoObj =>{
+        let res = await fetch(`http://127.0.0.1:8080/update-task/${todoObj.id}`, {
+            method:"POST",
+            body: JSON.stringify(todoObj),
+            headers:{
+                "Content-type": "application/json"
+            }
+        });
+        let data = await res.json();
+        self.dataInitiliazed = false;
+        self.initializeData();
+        return data;
     }
 })
